62e5821f4d22b5b97d50433754a68cbd03e573f3
Constantine7266 <72450813+Constantine7266@users.noreply.github.com>
2023-09-09 14:28:16 +0300

    C18.1_2.2
    Попытка применить пакет

--- Упр/18.1/18.1_2.py
+++ Упр/18.1/18.1_2.py
@@ -5,7 +5,7 @@ D = int(input(" Ведите число от 1 до 5:"))
 B = input(" Веди букву:")
 if D == 3:
     print("Угадал")
-elif D < 0:
+elif D == 0:
     print(" Не угодал ")
 elif D > 3:
     print(" Близко ")
@@ -13,3 +13,4 @@ elif D < 3:
     print(" Горечо ")
 else:
     print(B)
+print("Конец")
