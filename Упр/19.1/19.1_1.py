88ae92a409f81b11ef426a7d3e392b98764ab749
Nikolay38744 <97681446+Nikolay38744@users.noreply.github.com>
2023-09-12 16:53:58 +0300

    С19.1_1.2
    Что сохранилось в пакете
    
    Co-authored-by: Constantine7266 <72450813+Constantine7266@users.noreply.github.com>

--- Упр/19.1/19.1_1.py
+++ Упр/19.1/19.1_1.py
@@ -4,9 +4,10 @@ print("Угадай число до 4")
 A = int(input("Ведите число:"))
 if (A == 1) and ( 1 == 1):
     print("Угадал1")
-elif A == 2 and 2 == 2:
+elif A != 2 and 2 == 2:
     print("Не угадал2")
-elif A == 3 and 3 == 3:
+elif A == 1 and 3 == 3:
     print("Не угадал3")
 else:
     print("И опять не угадал")
+print("Конец игры")
