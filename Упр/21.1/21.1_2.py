175acf2612c37c25ff0eb3b7a9dc8a8f7f092045
Constantine7266 <7266@bk.ru>
2023-09-26 19:44:44 +0300

    C21.1_2.1

--- Упр/21.1/21.1_2.py
+++ Упр/21.1/21.1_2.py
@@ -1,1 +1,8 @@
 """ Напиши программу, при воде числа в которую выдаст название фрукта."""
+
+fruits = ["Apple", "Cherry", "Banana"]
+A = int(input())
+if A < 0 or A > 2:
+    print("Не угадал")
+elif A > 0 or A < 2:
+    print(fruits[A])
